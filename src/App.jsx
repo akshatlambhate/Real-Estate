7e82ef64ec75b6474301e86f0e81325146b40b2b
Akshat Lambhate <akshatlambhate180302@gmail.com>
2025-01-22 20:22:19 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-
+import {BrowserRouter , Routes , Route} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 function App() {
  
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/sign-in" element={<SignIn />}></Route>
+      <Route path="/sign-out" element={<SignOut />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
 
