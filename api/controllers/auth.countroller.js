4a17cdb1f107e1deece3ff14f0cd9fe00206f01d
Akshat Lambhate <akshatlambhate180302@gmail.com>
2025-01-24 14:01:37 +0530

    complete sign in page functionality

--- api/controllers/auth.countroller.js
+++ api/controllers/auth.countroller.js
@@ -30,7 +30,7 @@ export const signin = async(req,res,next) =>{
         res.cookie('access_token',token,{httpOnly: true,}).status(200).json(rest);
 
     } catch (error) {
-        next(error) 
+        next(error);
     }
 
 
