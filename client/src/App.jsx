67b6bf067e9d8f8b2b73c734b4124626961b85d4
Akshat Lambhate <akshatlambhate180302@gmail.com>
2025-01-24 23:03:48 +0530

    update the Header and make the profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import './index.css'; 
 import SignOut from "./pages/SignOut.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 function App() {
  
   return (
@@ -18,7 +19,9 @@ function App() {
       <Route path="/sign-up" element={<SignUp/>}></Route>
       <Route path="/sign-out" element={<SignOut/>}></Route>
       <Route path="/about" element={<About />}></Route>
+      <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />}></Route>
+      </Route>
 
     </Routes>
     
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import logo from '../assets/logo.png'
 import SearchIcon from '@mui/icons-material/Search';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function Header() {
+    const {currentUser} = useSelector(state =>state.user.user);
   return (
     <header className='flex h-32 gap-10 justify-evenly border items-center'>
         <div className=" logo h-10 w-10">
@@ -12,7 +14,6 @@ function Header() {
             </Link>
         </div>
         <div className="search">
-
             <form action="" className='  border border-gray-700  rounded-lg ' >
                 <input className=' w-24 sm:w-64 h-10 rounded-3xl placeholder: ml-5 focus:outline-none' type="text" placeholder='Search...'/>
                 <SearchIcon className='mr-5'/>
@@ -20,11 +21,17 @@ function Header() {
 
         </div>
         <div className="menu">
-            <ul className='flex gap-8   font-semibold '>
+            <ul className='flex gap-8 font-semibold '>
                 <li><Link to='/'>HOME</Link></li>
                 <li><Link to='/about'>ABOUT</Link></li>
-                <li><Link to='/sign-in'>SIGN-IN</Link></li>
-                
+              <Link to='/profile'>
+             { currentUser ?(<img className='rounded-full h-10 w-10 object-cover' src={currentUser.avatar} alt='profile'/>
+             ):(
+                <li>SIGN-IN</li>
+             )
+             }
+          </Link>
+ 
 
             </ul>
         </div>
