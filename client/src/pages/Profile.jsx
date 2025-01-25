c208a861b89633c83a627bb179d2068ffb9d120d
Akshat Lambhate <akshatlambhate180302@gmail.com>
2025-01-25 13:51:04 +0530

    cerate user update api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
 
 function Profile() {
+  const fileRef = useRef(null);
      const {currentUser } = useSelector((state) =>state.user.user)
+     const [formData, setformData] = useState({});
   return (
     <div className=' p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center'>Profile</h1>
       <form className='flex flex-col gap-5' >
-        <img src={currentUser.avatar} className='rounded-full h-24 w-24 object-cover curser-pointer self-center mt-2' alt="profile" />
+        <input type="file"  ref={fileRef} hidden accept='image/*'/>
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} className='rounded-full h-24 w-24 object-cover curser-pointer self-center mt-2' alt="profile" />
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' />
         <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' />
         <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' />
